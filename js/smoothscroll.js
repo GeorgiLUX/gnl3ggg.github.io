e3c1ddd560db2072a5ec9b162b0b7c9be8af2e17
gnl3ggg <georgilazarovux@gmail.com>
2017-10-02 22:29:41 +0300

    portfolio number 2 overhaul 3
    New overhauled website

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -8,28 +8,8 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
    // animate
    $('html, body').animate({
-       scrollTop: $(this.hash).offset().top -50
-     }, 700, function(){
-
-       // when done, add hash to url
-       // (default click behaviour)
-       window.location.hash = hash;
-     });
-
-});
-
-$("a.navbar-brand[href^='#']").on('click', function(e) {
-
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(this.hash).offset().top -50
-     }, 700, function(){
+       scrollTop: $(hash).offset().top
+     }, 355, function(){
 
        // when done, add hash to url
        // (default click behaviour)
