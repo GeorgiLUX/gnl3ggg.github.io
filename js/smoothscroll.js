e38b8e8726d08aa0173d16589638308d5f1e300d
gnl3ggg <georgilazarovux@gmail.com>
2017-11-06 16:39:37 +0200

    portfolio 3.6

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,23 +1,19 @@
-<script>
-var set=0;
-function animatescroll(x,y){
-if(set==0){
-var val71=x/500;
-var val72=0;
-var val73=0;
-var setin=0;
-set=1;
-var interval=setInterval(function(){
-if(setin==0){
-val72++;
-val73+=x/500;
-if(val72==500){
-val73=0;
-interval=clearInterval(interval);
-}
-document.getElementById(y).scrollTop=val73;
-}
-},1);
-}
-}
-</script>
+$(".navbar.navbar-custom.navbar-inverse.navbar-static-top ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50
+     }, 800, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
